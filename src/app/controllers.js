ab832df878b88b35faa29965432d9e543f21f090
Ian McCallum <ianfmc@gmail.com>
2015-07-23 09:17:07 -0700

    add D3 code to main controller

--- src/app/controllers.js
+++ src/app/controllers.js
@@ -55,4 +55,30 @@ angular.module('d3Playground')
     angular.forEach($scope.awesomeThings, function(awesomeThing) {
       awesomeThing.rank = Math.random();
     });
+
+    var body = d3.select('body');
+
+    var w = 1024;
+    var h =  768;
+
+    var paddingWidth = 5;
+
+    var spots = [ {type: 'pass', x: 200, y: 100},
+                  {type: 'pass', x: 250, y: 300},
+                  {type: 'shot', x: 800, y: 400},
+                  {type: 'pass', x: 540, y: 350},
+                  {type: 'pass', x: 600, y: 200},
+                  {type: 'shot', x: 300, y: 300}];
+
+    var svg = body.append('svg');
+
+    svg.attr('width', w);
+    svg.attr('height', h);
+
+    var leftScoreBox = svg.append('rect')
+      .attr('x', paddingWidth)
+      .attr('y', paddingWidth)
+      .attr('width', 100)
+      .attr('height', 100)
+      .style('fill', d3.rgb( 200, 0, 0));
   });
