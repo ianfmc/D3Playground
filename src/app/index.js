a097673d10db104ad5e366448592034afa8874c5
Ian McCallum <ianfmc@gmail.com>
2015-07-23 09:16:40 -0700

    remove legacy code

--- src/app/index.js
+++ src/app/index.js
@@ -11,16 +11,3 @@ angular.module('d3Playground', ['d3', 'ngAnimate', 'ngCookies', 'ngTouch', 'ngSa
         redirectTo: '/'
       });
   });
-
-angular.module('d3', [])
-  .factory('d3Service', ['$document', '$q', '$rootScope',
-    function($document, $q, $rootScope) {}]);
-
-angular.module('d3Playground.directives', ['d3'])
-.directive('tank', ['d3Service', function(d3Service) {
-  return {
-      link: function(scope, element, attrs) {
-        d3Service.d3().then(function(d3) {
-        });
-      }};
-    }]);
