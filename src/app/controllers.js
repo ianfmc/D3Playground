403ef1a0b16925e4f31c0b2cdf2c6ea961e2b626
Ian McCallum <ianfmc@gmail.com>
2015-07-23 09:27:49 -0700

    add all controller to single file

--- src/app/controllers.js
+++ src/app/controllers.js
@@ -55,7 +55,7 @@ angular.module('d3Playground')
     angular.forEach($scope.awesomeThings, function(awesomeThing) {
       awesomeThing.rank = Math.random();
     });
-
+/*
     var body = d3.select('body');
 
     var w = 1024;
@@ -81,4 +81,84 @@ angular.module('d3Playground')
       .attr('width', 100)
       .attr('height', 100)
       .style('fill', d3.rgb( 200, 0, 0));
+
+      var svg = body.append('svg');
+
+    svg.attr('width', w);
+    svg.attr('height', h);
+
+    var leftScoreBox = svg.append('rect')
+      .attr('x', paddingWidth)
+      .attr('y', paddingWidth)
+      .attr('width', 100)
+      .attr('height', 100)
+      .style('fill', d3.rgb( 200, 0, 0));
+
+    var leftScoreText = svg.append('text')
+      .text('4')
+      .attr('x', paddingWidth + 50)
+      .attr('y', paddingWidth + 70)
+      .attr('class', 'score-text')
+      .attr('text-anchor', 'middle');
+
+    var rightScoreBox = svg.append('rect')
+      .attr('class', 'score-box')
+      .attr('x', (w-100)-paddingWidth)
+      .attr('y', paddingWidth)
+      .attr('width', 100)
+      .attr('height', 100)
+      .style('fill', d3.rgb( 0, 200, 0));
+
+    var rightScoreText = svg.append('text')
+      .text('2')
+      .attr('x', (w-100)-paddingWidth + 50)
+      .attr('y', paddingWidth + 70)
+      .attr('class', 'score-text')
+      .attr('text-anchor', 'middle');
+
+    var controlBox = svg.append('rect')
+      .attr('x', w/2 - 100)
+      .attr('y', paddingWidth)
+      .attr('width', 200)
+      .attr('height', 100)
+      .style('fill', 'grey');
+
+    var controlBoxText = svg.append('text')
+      .text('4:30')
+      .attr('x', w/2)
+      .attr('y', paddingWidth + 70)
+      .attr('text-anchor', 'middle')
+      .attr('font-family', 'sans-serif')
+      .attr('font-size', '60px')
+      .attr('fill', 'white');
+
+    var tank = svg.append('rect')
+      .attr('class', 'tank')
+      .attr('x', paddingWidth)
+      .attr('y', 100+(2*paddingWidth))
+      .attr('width', w-(2*paddingWidth))
+      .attr('height', (h-(100+(2*paddingWidth)+paddingWidth)));
+
+    var eventCircles = svg.selectAll('circle')
+      .data(spots)
+      .enter()
+      .append('circle')
+      .attr('cx', function (d) { return d.x + paddingWidth; })
+      .attr('cy', function (d) { return d.y + 100 + 2*paddingWidth; })
+      .attr('r', '30')
+      .style('fill', function(d, i) {
+        return 'rgba(255, 255, 255, ' + (5-i)*0.2 + ')';
+      })
+      .style('stroke', function(d, i) {
+        return 'rgba(0, 0, 0, ' + (5-i)*0.2 + ')';
+      })
+      .style('stroke-width', function(d, i) {
+        if (d.type === 'pass') {
+          return '0';
+        }
+        else {
+          return '4';
+        }
+*/
+
   });
