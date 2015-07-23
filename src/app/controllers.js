eb1b3088d5fa95f0e81b55d430398e4749a6fe66
Ian McCallum <ianfmc@gmail.com>
2015-07-23 12:39:17 -0700

    Change order of element appending; add lines for middle, 5m, 2m and goals

--- src/app/controllers.js
+++ src/app/controllers.js
@@ -8,18 +8,18 @@ angular.module('d3Playground')
     var w = 1024;
     var h =  768;
 
+    var circleSize = 25;
+
     var paddingWidth = 5;
 
-    var spots = [ {player: '1', type: 'pass', x: 200, y: 100} ];
-
-    /*
+    var spots = [ {player: '1', type: 'pass', x: 200, y: 100},    
                   {player: '2', type: 'pass', x: 250, y: 300},
                   {player: '3', type: 'shot', x: 800, y: 400},
                   {player: '4', type: 'pass', x: 540, y: 350},
                   {player: '5', type: 'pass', x: 600, y: 200},
-                  {player: '6', type: 'shot', x: 300, y: 300}];
+                  {player: '6', type: 'shot', x: 300, y: 300} ];
 
-                  */
+                  /**/
 
     var svg = body.append('svg');
 
@@ -27,7 +27,136 @@ angular.module('d3Playground')
     svg.attr('height', h);
     svg.style('border', '2px black solid');
 
-    /* var Left Score Box */
+    /* Tank */
+
+    svg.append('rect')
+      .attr('class', 'tank')
+      .attr('x', paddingWidth)
+      .attr('y', 100+(2*paddingWidth))
+      .attr('width', w-(3*paddingWidth))
+      .attr('height', (h-(100+(3*paddingWidth)+paddingWidth)));
+
+    /* Center Line*/
+
+    svg.append('line')
+      .attr('x1', w*0.5)
+      .attr('y1', 100+2*paddingWidth)
+      .attr('x2', w*0.5)
+      .attr('y2', h-(2*paddingWidth))
+      .style('stroke-width', 10)
+      .style('stroke', 'white')
+      .style('opacity', '0.25')
+      .style('stroke-dasharray', ('20,5'));
+
+    /* Left 5m */
+
+    svg.append('line')
+      .attr('x1', w*0.15)
+      .attr('y1', 100+2*paddingWidth)
+      .attr('x2', w*0.15)
+      .attr('y2', h-(2*paddingWidth))
+      .style('stroke-width', 10)
+      .style('stroke', 'yellow')
+      .style('opacity', '0.25')
+      .style('stroke-dasharray', ('20,5'));
+
+    /* Right 5m */
+
+    svg.append('line')
+      .attr('x1', w*0.85)
+      .attr('y1', 100+2*paddingWidth)
+      .attr('x2', w*0.85)
+      .attr('y2', h-(2*paddingWidth))
+      .style('stroke-width', 10)
+      .style('stroke', 'yellow')
+      .style('opacity', '0.25')
+      .style('stroke-dasharray', ('20,5'));
+
+    /* Left 2m */
+
+    svg.append('line')
+      .attr('x1', w*0.05)
+      .attr('y1', 100+2*paddingWidth)
+      .attr('x2', w*0.05)
+      .attr('y2', h-(2*paddingWidth))
+      .style('stroke-width', 10)
+      .style('stroke', 'red')
+      .style('opacity', '0.25')
+      .style('stroke-dasharray', ('20,5'));
+
+    /* Right 2m */
+
+    svg.append('line')
+      .attr('x1', w*0.95)
+      .attr('y1', 100+2*paddingWidth)
+      .attr('x2', w*0.95)
+      .attr('y2', h-(2*paddingWidth))
+      .style('stroke-width', 10)
+      .style('stroke', 'red')
+      .style('opacity', '0.25')
+      .style('stroke-dasharray', ('20,5'));
+
+
+    /* Left Goal */
+
+    svg.append('line')
+      .attr('x1', w*0.01)
+      .attr('y1', 50+(h*0.5)-50)
+      .attr('x2', w*0.01)
+      .attr('y2', 50+(h*0.5)+50)
+      .style('stroke-width', 20)
+      .style('stroke', 'white')
+
+    /* Right Goal */
+
+    svg.append('line')
+      .attr('x1', w*0.99)
+      .attr('y1', 50+(h*0.5)-50)
+      .attr('x2', w*0.99)
+      .attr('y2', 50+(h*0.5)+50)
+      .style('stroke-width', 20)
+      .style('stroke', 'white')
+
+    /* Event Circles */
+
+    svg.selectAll('circle')
+      .data(spots)
+      .enter()
+      .append('circle')
+      .attr('cx', function (d) { return d.x + paddingWidth; })
+      .attr('cy', function (d) { return d.y + 100 + 2*paddingWidth; })
+      .attr('r', circleSize)
+      .style('fill', function(d, i) {
+        return 'rgba(255, 255, 255, ' + (5-i)*0.2 + ')';
+      })
+      .style('stroke', function(d, i) {
+        return 'rgba(0, 0, 0, ' + (5-i)*0.2 + ')';
+      })
+      .style('stroke-width', function(d) {
+        if (d.type === 'pass') {
+          return '0';
+        }
+        else {
+          return '4';
+        }});
+
+    /* var Player Numbers */
+
+    svg.selectAll('text')
+      .data(spots)
+      .enter()
+      .append('text')
+      .text(function(d) {return d.player; })
+      .attr('x', function (d) { return d.x + paddingWidth; })
+      .attr('y', function (d) { return d.y + 100 + 4*paddingWidth; })
+      .attr('font-family', 'arial')
+      .attr('font-size', '30px')
+      .style('fill', function(d, i) {
+        return 'rgba(0, 0, 0, ' + (5-i)*0.2 + ')';
+      })
+      .attr('text-anchor', 'middle');
+
+          /* var Left Score Box */
 
     svg.append('rect')
       .attr('x', paddingWidth)
@@ -84,51 +213,6 @@ angular.module('d3Playground')
       .attr('font-size', '60px')
       .attr('fill', 'white');
 
-    /* var Tank */
-
-    svg.append('rect')
-      .attr('class', 'tank')
-      .attr('x', paddingWidth)
-      .attr('y', 100+(2*paddingWidth))
-      .attr('width', w-(3*paddingWidth))
-      .attr('height', (h-(100+(3*paddingWidth)+paddingWidth)));
-
-    /* var Event Circles */
-
-    svg.selectAll('circle')
-      .data(spots)
-      .enter()
-      .append('circle')
-      .attr('cx', function (d) { return d.x + paddingWidth; })
-      .attr('cy', function (d) { return d.y + 100 + 2*paddingWidth; })
-      .attr('r', '30')
-      .style('fill', function(d, i) {
-        return 'rgba(255, 255, 255, ' + (5-i)*0.2 + ')';
-      })
-      .style('stroke', function(d, i) {
-        return 'rgba(0, 0, 0, ' + (5-i)*0.2 + ')';
-      })
-      .style('stroke-width', function(d) {
-        if (d.type === 'pass') {
-          return '0';
-        }
-        else {
-          return '4';
-        }});
-
-    /* var Player Numbers */
-
-    svg.selectAll('text')
-      .data(spots)
-      .enter()
-      .append('text')
-      .text(function(d) {return d.player; })
-      .attr('x', function (d) { return d.x + paddingWidth; })
-      .attr('y', function (d) { return d.y + 100 + 4*paddingWidth; })
-      .attr('font-family', 'arial')
-      .attr('font-size', '30px')
-      .attr('fill', 'black')
-      .attr('text-anchor', 'middle');
 });
 
 angular.module('d3Playground')
